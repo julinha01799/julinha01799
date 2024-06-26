7ba6cc411d6ea6723ba6a3eca2f6c429bfbe5867
julinha01799 <juliadefreitas017@gmail.com>
2024-06-26 09:42:34 -0300

    adiconando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    stroke ("blue");
+    fill("red")
+  }
+  
+    if(mouseIspressed){
+      rect(mouseX, mouseY, 20, 35);
+    }
+  
+  }
